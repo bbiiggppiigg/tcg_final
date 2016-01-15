5940f49af798707eed277a0d7a10856271f1316d
=bbiiggppiigg <=bbiiggppiigg@gmail.com>
2016-01-15 15:46:08 +0800

    fix table

--- gen_hash_array.cpp
+++ gen_hash_array.cpp
@@ -7,8 +7,13 @@ using namespace std;
 int main(){
 	srand(time(NULL));
 	printf("unsigned int zobrist_table[32][15] = {");
-	for (int i =0 ; i < 32 ; i++){
-		cout<<"{"<<(unsigned int) rand();
+	cout<<"{"<<(unsigned int) rand();
+	for(int j =0;  j  < 15 ;j ++){
+		cout<<","<<(unsigned int) rand();
+	}
+	cout<<"}";
+	for (int i =1 ; i < 32 ; i++){
+		cout<<",{"<<(unsigned int) rand();
 		for(int j =0;  j  < 15 ;j ++){
 			cout<<","<<(unsigned int) rand();
 		}
